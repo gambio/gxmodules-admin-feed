7573c286a628a8505f419822bb6b9dc2a42319ef
Hendrik Mennen <hendrik.mennen@marmalade.de>
2024-05-16 10:16:47 +0200

    replace isset with array_key_exists

--- src/GXModules/Gambio/AdminFeed/Services/ShopInformation/Serializer/ShopDetailsSerializer.php
+++ src/GXModules/Gambio/AdminFeed/Services/ShopInformation/Serializer/ShopDetailsSerializer.php
@@ -31,18 +31,18 @@ class ShopDetailsSerializer
     public function serialize(ShopDetails $shopDetails)
     {
         $json = [
-            'version'         => $shopDetails->version(),
-            'url'             => $shopDetails->url(),
-            'key'             => $shopDetails->key(),
-            'languages'       => $shopDetails->languages(),
+            'version' => $shopDetails->version(),
+            'url' => $shopDetails->url(),
+            'key' => $shopDetails->key(),
+            'languages' => $shopDetails->languages(),
             'defaultLanguage' => $shopDetails->defaultLanguage(),
-            'countries'       => $shopDetails->countries(),
+            'countries' => $shopDetails->countries(),
         ];
-        
+
         return $json;
     }
-    
-    
+
+
     /**
      * Returns a new ShopDetails instance by using the data of a given array or json strings.
      *
@@ -55,21 +55,23 @@ class ShopDetailsSerializer
         if (!is_array($json)) {
             $json = json_decode($json, true);
         }
-        
-        if (!isset($json['version'])
-            || !isset($json['url'])
-            || !isset($json['key'])
-            || !isset($json['languages'])
-            || !isset($json['defaultLanguage'])
-            || !isset($json['countries'])) {
+
+        if (!array_key_exists('version', $json)
+            || !array_key_exists('url', $json)
+            || !array_key_exists('key', $json)
+            || !array_key_exists('languages', $json)
+            || !array_key_exists('defaultLanguage', $json)
+            || !array_key_exists('countries', $json)) {
             throw new InvalidArgumentException('Given argument is invalid. Needed property is missing.');
         }
-        
-        return ShopDetails::create($json['version'],
-                                   $json['url'],
-                                   $json['key'],
-                                   $json['languages'],
-                                   $json['defaultLanguage'],
-                                   $json['countries']);
+
+        return ShopDetails::create(
+            $json['version'],
+            $json['url'],
+            $json['key'],
+            $json['languages'],
+            $json['defaultLanguage'],
+            $json['countries']
+        );
     }
 }
